6c36db2bc78b467d0be15c782896500e6c223027
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-05 21:47:26 -0500

    Refactor DashboardComponent and add signOut

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { FirebaseAuthService } from '../shared/services/firebase/firebase-auth.service';
 import { LocalizationService } from '../shared/services/localization/localization.service';
 import { ThemeService } from '../shared/services/themes/theme.service';
 import { getLocalStorageBooleanItem } from '../shared/utils/utils';
@@ -14,17 +16,20 @@ import { getLocalStorageBooleanItem } from '../shared/utils/utils';
 export class DashboardComponent implements OnInit {
 
   isLightMode: boolean = false;
-
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map((result: any) => result?.matches),
       shareReplay()
     );
+  user!: any;
+  user$: Observable<any> = this.authService.auth.user;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
     public locale: LocalizationService,
-    private themeService: ThemeService,
+    public themeService: ThemeService,
+    private authService: FirebaseAuthService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -44,6 +49,11 @@ export class DashboardComponent implements OnInit {
     const currentLanguage = this.locale.getCurrentLanguage();
     const lang = currentLanguage === 'es-CO' ? 'en-US' : 'es-CO';
     await this.locale.useLanguage(lang);
+  }
+
+  async signOut(): Promise<void> {
+    await this.authService.signOut();
+    this.router.navigate(['/auth/sign-in']);
   }
 
 
