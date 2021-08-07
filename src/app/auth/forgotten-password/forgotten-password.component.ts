27265d7f2ce9e946d4206d1cee9c8906b7ee6a75
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-06 23:27:59 -0500

    Add ForgottenPasswordComponent

---
+++ src/app/auth/forgotten-password/forgotten-password.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { FirebaseAuthService } from 'src/app/shared/services/firebase/firebase-auth.service';
+import { LocalizationService } from 'src/app/shared/services/localization/localization.service';
+
+@Component({
+  selector: 'nm-forgotten-password',
+  templateUrl: './forgotten-password.component.html',
+  styleUrls: ['./forgotten-password.component.scss']
+})
+export class ForgottenPasswordComponent implements OnInit {
+
+  form!: FormGroup;
+  isLoading!: boolean;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: FirebaseAuthService,
+    private snackBar: MatSnackBar,
+    private locale: LocalizationService,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required]],
+    });
+  }
+
+  async onSubmit(): Promise<void> {
+    if (this.form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    const { email } = this.form.value;
+    await this.auth.sendPasswordResetEmail(email);
+    this.isLoading = false;
+    const message = this.locale.translate('auth.password-reset-message');
+    const snackBarRef = this.snackBar.open(`${message} ${email}`, undefined, {
+      duration: 4000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+    snackBarRef.afterDismissed().subscribe(
+      () => this.router.navigate(['/auth/sign-in'])
+    );
+  }
+}
