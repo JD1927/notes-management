d2ed4a21c694c1abe40492e735bb45397520af78
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-07 18:50:25 -0500

    Add inital UsersComponent

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-
+import { InternationalizationModule } from '../shared/modules/internationalization/internationalization.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import { InternationalizationModule } from '../shared/modules/internationalization/internationalization.module';
-import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 
 @NgModule({
