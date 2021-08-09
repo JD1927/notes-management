6d687d7070080e8be9a4a46105a15543b6b90430
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-08 21:55:51 -0500

    Add ApartmentsService

---
+++ src/app/shared/services/firebase/apartments.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Apartment } from '../../models/apartments.model';
+import { BaseService } from './base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApartmentsService extends BaseService<Apartment> {
+
+  constructor(afs: AngularFirestore) {
+    super('apartments', afs);
+  }
+}
