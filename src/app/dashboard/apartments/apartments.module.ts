9f0cf8a9451214e0f6fa1e4db922d78353743423
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-08 21:37:41 -0500

    Refactor and add modules in the ApartmentsModule

--- src/app/dashboard/apartments/apartments.module.ts
+++ src/app/dashboard/apartments/apartments.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { InternationalizationModule } from 'src/app/shared/modules/internationalization/internationalization.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { ApartmentsRoutingModule } from './apartments-routing.module';
 import { ApartmentsComponent } from './apartments.component';
-import { InternationalizationModule } from 'src/app/shared/modules/internationalization/internationalization.module';
-
 
 @NgModule({
   declarations: [
@@ -13,7 +24,21 @@ import { InternationalizationModule } from 'src/app/shared/modules/international
   imports: [
     CommonModule,
     ApartmentsRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatCardModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatTableModule,
     InternationalizationModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
   ]
 })
 export class ApartmentsModule { }
