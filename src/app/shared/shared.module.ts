b57d083adb6d995a2f7d30ce09ee513c8414902c
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-08 21:35:10 -0500

    Refactor SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,15 +4,26 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { InternationalizationModule } from './modules/internationalization/internationalization.module';
 import { TimeFormatPipe } from './pipes/time-format.pipe';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { ConfirmDialogService } from './services/dialog/confirm-dialog.service';
 
 
 @NgModule({
-  providers: [TimeFormatPipe],
   imports: [
     CommonModule,
     MatDialogModule,
     MatButtonModule,
     InternationalizationModule,
   ],
+  declarations: [
+    ConfirmDialogComponent,
+  ],
+  exports: [
+    ConfirmDialogComponent,
+  ],
+  providers: [
+    ConfirmDialogService,
+    TimeFormatPipe
+  ]
 })
 export class SharedModule { }
