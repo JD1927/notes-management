c021a445e8d0ef30b0a11e8c952c065540ff00d2
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-08 21:34:50 -0500

    Add ConfirmDialogService

---
+++ src/app/shared/services/dialog/confirm-dialog.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
+import { ConfirmDialog } from '../../models/dialog.model';
+
+@Injectable()
+export class ConfirmDialogService {
+
+  constructor(private dialog: MatDialog) { }
+
+  public open(options: ConfirmDialog): MatDialogRef<ConfirmDialogComponent> {
+    return this.dialog.open(ConfirmDialogComponent, {
+      minHeight: '200px',
+      width: '400px',
+      data: {
+        title: options.title,
+        message: options.message,
+        cancelText: options.cancelText,
+        confirmText: options.confirmText
+      }
+    });
+  }
+  public confirmed(dialogRef: MatDialogRef<ConfirmDialogComponent>): Observable<any> {
+    return dialogRef.afterClosed().pipe(take(1), map(res => {
+      return res;
+    }));
+  }
+}
