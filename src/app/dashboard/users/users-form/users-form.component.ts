71e2cb1e80bb4c18bf50b46e00199ace24a59a80
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-09 23:55:34 -0500

    Add initial steps in User Update feature

--- src/app/dashboard/users/users-form/users-form.component.ts
+++ src/app/dashboard/users/users-form/users-form.component.ts
@@ -1,10 +1,10 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { TimeFormatPipe } from 'src/app/shared/pipes/time-format.pipe';
-import { NotesService } from 'src/app/shared/services/firebase/notes.service';
+import { Apartment } from 'src/app/shared/models/apartments.model';
+import { ApartmentsService } from 'src/app/shared/services/firebase/apartments.service';
+import { FirebaseAuthService } from 'src/app/shared/services/firebase/firebase-auth.service';
 import { LocalizationService } from 'src/app/shared/services/localization/localization.service';
 import { ThemeService } from 'src/app/shared/services/themes/theme.service';
 import { EMAIL_REGEX } from 'src/app/shared/utils/utils';
@@ -20,23 +20,38 @@ export class UsersFormComponent implements OnInit {
   isLoading!: boolean;
   isDesktop$: Subscription = new Subscription();
   isDesktop!: boolean;
+  _apartments$: Subscription = new Subscription();
+  apartments: Apartment[] = [];
 
   constructor(
     private fb: FormBuilder,
     public themeConfig: ThemeService,
     private router: Router,
     private localeService: LocalizationService,
+    private authService: FirebaseAuthService,
+    private aptoService: ApartmentsService,
   ) { }
 
   ngOnInit(): void {
     this.createForm();
+    this.getApartments();
   }
 
   createForm(): void {
     this.form = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(60)]],
-      email: ['', [Validators.required, Validators.pattern(EMAIL_REGEX)]],
+      name: ['', [Validators.required, Validators.maxLength(60)]],
+      aptoNumber: ['', [Validators.required]],
     });
+  }
+
+  getApartments(): void {
+    this.isLoading = true;
+    this._apartments$ = this.aptoService.list().subscribe(
+      (res) => {
+        this.apartments = [...res];
+        this.isLoading = false;
+      }
+    )
   }
 
   onSubmit(): void {
