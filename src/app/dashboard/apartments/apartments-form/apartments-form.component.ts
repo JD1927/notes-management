95f496e4185651958267ca5509b5fee8f21c0d6b
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-11 22:07:43 -0500

    Add Update function to the ApartmentForm

--- src/app/dashboard/apartments/apartments-form/apartments-form.component.ts
+++ src/app/dashboard/apartments/apartments-form/apartments-form.component.ts
@@ -1,18 +1,9 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { DateAdapter } from '@angular/material/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import * as moment from 'moment';
-import { Moment } from 'moment';
 import { Subscription } from 'rxjs';
-import { skipWhile } from 'rxjs/operators';
 import { Apartment } from 'src/app/shared/models/apartments.model';
-import { Note } from 'src/app/shared/models/notes.model';
-import { TimeFormatPipe } from 'src/app/shared/pipes/time-format.pipe';
 import { ApartmentsService } from 'src/app/shared/services/firebase/apartments.service';
-import { NotesService } from 'src/app/shared/services/firebase/notes.service';
-import { LocalizationService } from 'src/app/shared/services/localization/localization.service';
 import { ThemeService } from 'src/app/shared/services/themes/theme.service';
 
 @Component({
@@ -26,6 +17,7 @@ export class ApartmentsFormComponent implements OnInit, OnDestroy {
   isLoading!: boolean;
   isDesktop!: boolean;
   _route$: Subscription = new Subscription();
+  apartment!: Apartment;
 
   constructor(
     private fb: FormBuilder,
@@ -58,6 +50,7 @@ export class ApartmentsFormComponent implements OnInit, OnDestroy {
       return;
     }
     this.aptoService.get(id).subscribe((apto) => {
+      this.apartment = { ...apto };
       this.form.controls['aptoNumber'].setValue(apto.aptoNumber);
       this.form.updateValueAndValidity();
       this.isLoading = false;
@@ -80,6 +73,20 @@ export class ApartmentsFormComponent implements OnInit, OnDestroy {
       ...this.form.value,
     };
     await this.aptoService.add(item);
+    this.isLoading = false;
+    this.router.navigate(['/home/apartments']);
+  }
+
+  async onUpdate(): Promise<void> {
+    if (this.form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    let item: Apartment = {
+      ...this.apartment,
+      ...this.form.value,
+    };
+    await this.aptoService.update(item);
     this.isLoading = false;
     this.router.navigate(['/home/apartments']);
   }
