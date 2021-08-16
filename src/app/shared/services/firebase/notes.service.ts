3ff9a50cc2ec419bcffabf493d46004ad3234314
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-15 23:57:17 -0500

    Add new method to the NotesService

--- src/app/shared/services/firebase/notes.service.ts
+++ src/app/shared/services/firebase/notes.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Note } from '../../models/notes.model';
 import { BaseService } from './base.service';
 
@@ -8,7 +10,15 @@ import { BaseService } from './base.service';
 })
 export class NotesService extends BaseService<Note> {
 
+  _notes$: Subscription = new Subscription();
+
   constructor(afs: AngularFirestore) {
     super('notes', afs);
+  }
+
+  getNotesByApartment(aptoID: string): Observable<Note[]> {
+    return this.afs.collection<Note>(
+      'notes', ref => ref.where('aptoID', '==', aptoID)
+    ).valueChanges();
   }
 }
