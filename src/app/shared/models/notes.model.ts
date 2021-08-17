67cca7b55dbc8975ed18e38fcf8aa267a9bfe595
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-16 23:27:10 -0500

    Update Notes Model

--- src/app/shared/models/notes.model.ts
+++ src/app/shared/models/notes.model.ts
@@ -2,6 +2,9 @@ export interface Note {
   id?: string;
   title: string;
   description?: string;
+  aptoID: string;
+  apartment: string;
+  userID: string;
   date: string;
   time: string;
   createdAt: string;
