8f4063d39c989bebe428df1c6d0628cd005e0161
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-18 23:50:34 -0500

    Add some changes for NoteList

--- src/app/dashboard/notes/notes-list/notes-list.component.ts
+++ src/app/dashboard/notes/notes-list/notes-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
+import { User } from 'src/app/shared/models/auth.model';
 import { Note } from 'src/app/shared/models/notes.model';
+import { FirebaseAuthService } from 'src/app/shared/services/firebase/firebase-auth.service';
 import { NotesService } from 'src/app/shared/services/firebase/notes.service';
 import { ThemeService } from 'src/app/shared/services/themes/theme.service';
 
@@ -13,28 +15,44 @@ export class NotesListComponent implements OnInit, OnDestroy {
   isLoading!: boolean;
   notes: Note[] = [];
   _notes$: Subscription = new Subscription();
+  _user$: Subscription = new Subscription();
+  user!: User;
 
   constructor(
     private notesService: NotesService,
     public themeConfig: ThemeService,
+    private authService: FirebaseAuthService,
   ) { }
 
   ngOnInit(): void {
-   this.getNoteList();
+    this.getCurrentUser();
+  }
+
+  async getCurrentUser(): Promise<void> {
+    try {
+      const userRef = await this.authService.getCurrentUser();
+      this.authService.get((userRef?.uid as string))
+        .subscribe((user) => {
+          this.user = { ...user };
+          this.getNotesByUser(user?.id);
+        });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   ngOnDestroy(): void {
     this._notes$.unsubscribe();
+    this._user$.unsubscribe();
   }
 
-  getNoteList(): void {
+  getNotesByUser(userID: string): void {
     this.isLoading = true;
-    this._notes$ = this.notesService.list()
-      .subscribe(
-        (res) => {
-          this.notes = [...res];
-          this.isLoading = false;
-        }
-      );
+    this._notes$ = this.notesService.getNotesByUser(userID).subscribe(
+      (res) => {
+        this.notes = [...res];
+        this.isLoading = false;
+      }
+    );
   }
 }
