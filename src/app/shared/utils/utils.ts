178565b26ed158f33dc39e40eb19e2e881923ad9
Juan Aguirre <j1927aguirre@gmail.com>
2021-08-27 23:53:47 -0500

    Remove local storage function for toggle theme

--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -1,8 +1,3 @@
 export const EMAIL_REGEX = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 export const SIMPLE_PASSWORD_REGEX = `(?=.*[a-z])(?=.*[0-9]).{6,}`;
 export const PASSWORD_REGEX = `(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{6,}`;
-
-export const getLocalStorageBooleanItem = (key: string): boolean => {
-  const item = localStorage.getItem(key);
-  return item === 'true';
-}
