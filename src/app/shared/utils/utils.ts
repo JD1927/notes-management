804dfbfc178bb004ee76e70e274e4d1194c2b8d2
Juan Aguirre <j1927aguirre@gmail.com>
2021-11-14 00:23:23 -0500

    Add new regex and isDesktop util function

--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -1,3 +1,15 @@
-export const EMAIL_REGEX = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+export const EMAIL_REGEX = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 export const SIMPLE_PASSWORD_REGEX = `(?=.*[a-z])(?=.*[0-9]).{6,}`;
 export const PASSWORD_REGEX = `(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{6,}`;
+export const PHONE_NUMBER = '[0-9]{10}';
+export const TABLET_REGEX = /(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i;
+export const MOBILE_REGEX =
+  /Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/;
+
+export const isDesktopDevice = (): boolean => {
+  const agent = navigator.userAgent;
+  if (TABLET_REGEX.test(agent) || MOBILE_REGEX.test(agent)) {
+    return false;
+  }
+  return true;
+};
