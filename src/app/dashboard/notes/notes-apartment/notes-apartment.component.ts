b70c91362e65aac8b06c310cf5d76f703e1e8585
Juan Aguirre <j1927aguirre@gmail.com>
2021-11-14 00:40:44 -0500

    Add WhatsApp feature when searching notes

--- src/app/dashboard/notes/notes-apartment/notes-apartment.component.ts
+++ src/app/dashboard/notes/notes-apartment/notes-apartment.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
 import { Apartment } from 'src/app/shared/models/apartments.model';
+import { User } from 'src/app/shared/models/auth.model';
 import { Note } from 'src/app/shared/models/notes.model';
 import { ApartmentsService } from 'src/app/shared/services/firebase/apartments.service';
+import { FirebaseAuthService } from 'src/app/shared/services/firebase/firebase-auth.service';
 import { NotesService } from 'src/app/shared/services/firebase/notes.service';
+import { TranslationService } from 'src/app/shared/services/translation/translation.service';
 
 @Component({
   selector: 'nm-notes-apartment',
   templateUrl: './notes-apartment.component.html',
-  styleUrls: ['./notes-apartment.component.scss']
+  styleUrls: ['./notes-apartment.component.scss'],
 })
 export class NotesApartmentComponent implements OnInit {
-
   form!: FormGroup;
   isLoading!: boolean;
   notes: Note[] = [];
@@ -23,8 +26,10 @@ export class NotesApartmentComponent implements OnInit {
   constructor(
     private notesService: NotesService,
     private aptoService: ApartmentsService,
+    private authService: FirebaseAuthService,
     private fb: FormBuilder,
-  ) { }
+    public translation: TranslationService
+  ) {}
 
   ngOnInit(): void {
     this.createForm();
@@ -43,34 +48,27 @@ export class NotesApartmentComponent implements OnInit {
 
   getNoteList(): void {
     this.isLoading = true;
-    this._notes$ = this.notesService.list()
-      .subscribe(
-        (res) => {
-          this.notes = [...res];
-          this.isLoading = false;
-        }
-      );
+    this._notes$ = this.notesService.list().subscribe((res) => {
+      this.notes = [...res];
+      this.isLoading = false;
+    });
   }
 
   getApartments(): void {
     this.isLoading = true;
-    this._apartments$ = this.aptoService.list().subscribe(
-      (res) => {
-        this.apartments = [...res];
-        this.isLoading = false;
-      }
-    );
+    this._apartments$ = this.aptoService.list().subscribe((res) => {
+      this.apartments = [...res];
+      this.isLoading = false;
+    });
   }
 
   getNotesByApartment(aptoID: string): void {
     this.isLoading = true;
-    this.notesService.getNotesByApartment(aptoID).subscribe(
-      (res) => {
-        this.notes = [...res];
-        this.isLoading = false;
-        this.notesService.deleteNotesByApartment(aptoID);
-      }
-    );
+    this.notesService.getNotesByApartment(aptoID).subscribe((res) => {
+      this.notes = [...res];
+      this.isLoading = false;
+      this.notesService.deleteNotesByApartment(aptoID);
+    });
   }
 
   onSubmit(): void {
@@ -79,5 +77,16 @@ export class NotesApartmentComponent implements OnInit {
     }
     const { apartment } = this.form.value;
     this.getNotesByApartment(apartment);
+  }
+
+  async onMessageOwner(note: Note): Promise<void> {
+    const noteOwner: User = await this.authService
+      .get(note?.userID)
+      .pipe(first())
+      .toPromise();
+
+    if (noteOwner?.phoneNumber) {
+      window.open(`https://wa.me/57${noteOwner?.phoneNumber}`, '_blank');
+    }
   }
 }
